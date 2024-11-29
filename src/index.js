c8d7bd974a8d280e6cca85a963bbef79a1781efa
Alfredina-A <daldinart@gmail.com>
2024-11-29 13:06:31 +0000

    adding all changes

---
+++ src/index.js
@@ -0,0 +1,10 @@
+console.log(1139)
+let num2 = 2
+console.log(num2);
+let num3 = 3;
+console.log(num3);
+console.log( num2%num3);
+
+let light = true; //boolean
+console.log(typeof(light))  //typeof determine datatype
+let 
